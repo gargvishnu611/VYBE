1124f662b8a4c8ff71cf76bf7fe68423d5e922c0
gargvishnu611 <gargvishnu611@gmail.com>
2026-09-12 10:07:14 +0530

    Bump VYBE production cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='vybe-v7';
+const CACHE='vybe-v8';
 const CORE=['./','./index.html','./style.css','./script.js','./app.js','./production-mode.js','./live-player.js','./vybe-config.js','./vybe-backend.js','./manifest.json','./icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('vybe-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
